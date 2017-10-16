3de6110cb5919fa43bb85b22b96ba0ac7b914315
AvA4EvR <AvA4EvR@hotmail.com>
2017-10-16 12:58:08 -0600

    Initial commit

---
+++ src/components/Server/ServerDetails.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p>Please select a server</p>
+        <button>Change to normal</button>
+    </div>
+
+
+</template>
+
+<script>
+    import {}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Server/Servers.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <li
+                    class="list-group-item"
+                    v-for="index in servers">
+                Server #{{ index.id }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default{
+        data: function() {
+            return {
+
+                servers: [
+
+                    { id: 1, status: 'Normal'},
+                    { id: 2, status: 'Critical'},
+                    { id: 3, status: 'Unknown'},
+                    { id: 4, status: 'Normal'}
+                ]
+
+            }
+        },
+    }
+
+</script>
+
+<style>
+
+</style>
